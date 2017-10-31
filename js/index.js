0f2266d28857140b1eb76efd00df5387d8beaa99
liaoyangsen <zhangkai@myhome.163.com>
2017-10-31 18:14:54 +0800

    添加模拟数据

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
+
+
+
+
 $(function () {
+
+	
+	
 	
 	var mySwiper = new Swiper ('.swiper-container', {
 		direction:'horizontal',
@@ -19,4 +26,20 @@ $(function () {
 	});
 
 
+	var arrs = new Array();
+	for(var i=0; i<6; i++){
+
+		var itemData = new Object();
+		itemData.imgSrc = "";
+		itemData.desc = "两大浏览器将停止自动播放视频，这对广告行业有什么影响？";
+		itemData.publishTime = 1509441932132;//毫秒做单位
+		itemData.commentCount = 10;
+		itemData.praiseCount = 15;	
+
+		arrs.push(itemData);
+	}
+
+
+
 });
+
