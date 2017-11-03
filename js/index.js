f468d0705c9f844e4a6708d78db3c0707e57d95f
liaoyangsen <zhangkai@myhome.163.com>
2017-11-03 11:11:59 +0800

    优化item-large ui

--- js/index.js
+++ js/index.js
@@ -131,26 +131,36 @@ function createItem(itemData, type, lineCount){
 	var praiseStr = '<span>'+itemData.praiseCount+'</span>';
 	//这里的class还是强依赖了，修改了css文件，这里也得跟着修改
 	var divCommentPraise = '<div class="item-normal-praise">' + commentStr +　praiseStr+'</div>';
+
+
+
 	var timeStr = '<span>刚刚</span>';
-	var divTimeCommentPraise = '<div class="item-time-comment-praise">'+ timeStr + divCommentPraise+'</div>';
+	var divTimeCommentPraise = '';
+	if(200 === type){
+		divTimeCommentPraise = '<div class="item-time-comment-praise item-large-time-comment-praise">'+ timeStr + divCommentPraise+'</div>';
+	} else{
+		divTimeCommentPraise = '<div class="item-time-comment-praise">'+ timeStr + divCommentPraise+'</div>';
+	}
 
 	var descStr = '<span>'+itemData.desc+'</span>';
-	var divTxt = '<div class="item-normal-txt">'+descStr+divTimeCommentPraise+'</div>';
+	// var divTxt = '<div class="item-normal-txt">'+descStr+divTimeCommentPraise+'</div>';
+	var divTxt = '<div class="item-normal-txt">'+descStr+'</div>';
 
 	var img = '<img src="'+itemData.imgSrc+'">';
 
 	var categoryStr = '<span>商业</span>';
-
 	var divCategory = divCategory = '<div class="item-normal-category">'+categoryStr+'</div>';
 
-	var aTag = '<a href="">'+divCategory+img+divTxt+'</a>';
+	var aTag = '<a href="">'+divCategory+img+divTxt+divTimeCommentPraise+'</a>';
 
 	if(200 === type){
 
+
+
 		if(4 === lineCount){
-			return '<div class="item-large">'+aTag+'</div>';					
+			return '<div class="tem-normal item-large">'+aTag+'</div>';					
 		}
-		return '<div class="item-large item-margin-right">'+aTag+'</div>';
+		return '<div class="item-normal item-large item-margin-right">'+aTag+'</div>';
 	} else{
 
 		if(4 === lineCount) {
